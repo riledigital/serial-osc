ad076eddcdc91131e4e6154778bdae88eff303ba
Ri <45131445+rl2999@users.noreply.github.com>
2020-11-18 19:28:23 -0500

    init

--- app.py
+++ app.py
@@ -2,18 +2,22 @@ import serial
 from pythonosc.udp_client import SimpleUDPClient
 
 CLIENT_IP = "127.0.0.1"
-PORT = 1337
+# CLIENT_IP = "localhost"
+OSC_PORT = 7000
 OSC_ADDRESS = "/arduino/sensor1"
 osc_client = SimpleUDPClient(CLIENT_IP, PORT)
+print(f"OSC UDP client sending to {CLIENT_IP} on port {OSC_PORT}, address: {OSC_ADDRESS}")
+print(f"Connecting to serial at {SERIAL_DEVICE} with baud {BAUD_RATE}")
+SERIAL_DEVICE = '/dev/tty.usbmodem14201'
+BAUD_RATE = 19200
 
-SERIAL_DEVICE = '/dev/ttyACM0'
-
-ser = serial.Serial(SERIAL_DEVICE)
+ser = serial.Serial(SERIAL_DEVICE, baudrate=BAUD_RATE,timeout=None)
 ser.flushInput()
 
 while True:
     try:
         ser_bytes = ser.readline()
+        # print(ser_bytes)
         decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
         # Send a float through OSC
         osc_client.send_message(OSC_ADDRESS, decoded_bytes)
